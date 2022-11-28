1e2a231648f7a54f500842db920d5eb9473f65fc
Rami Elsayed <ramielsayed123@outlook.com>
2022-11-28 21:20:45 +0000

    item entity changed to product

--- StoreService.Infrastructure/Entities/Product.cs
+++ StoreService.Infrastructure/Entities/Product.cs
@@ -6,19 +6,12 @@ using System.Threading.Tasks;
 
 namespace StoreService.Infrastructure.Entities
 {
-    public class Item
+    public class Product
     {
-        public Item(int sizeID, int colorID)
-        {
-            SizeID = sizeID;
-            ColorID = colorID;
-        }
 
         public int ID { get; set; }
-        public int SizeID { get; set; }
-        public int ColorID { get; set; }
         public int OrderID { get; set; }
-        public Order? ItemOrder { get; set; }
+        public Order? ProductOrder { get; set; }
 
         public ICollection<Color> Colors { get; set; } = new List<Color>();
         public ICollection<Size> Sizes { get; set; } = new List<Size>();
